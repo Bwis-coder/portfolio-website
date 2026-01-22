25bec4497bde202f645405935df1697feef9c4d0
Bwis <wisdomezekiel9@gamil.com>
2026-01-22 13:50:38 +0100

    correction

--- src/Components/HeaderFiles/Header.jsx
+++ src/Components/HeaderFiles/Header.jsx
@@ -5,18 +5,18 @@ export function Header() {
   return (
     <div className='header-container-main'>
       <header className='header-container' >
-        <p className='code-nmae'>BwisTech</p>
+        <p className='code-name'>BwisTech</p>
 
         <nav className='nav-container'>
-          <NavLink className={({isActive})=>{
-      return  isActive ? 'active-link' : ''
-        }} to='/'>Home</NavLink>
-          <NavLink className={({isActive})=>{
-       return isActive ? 'active-link' : ''
-        }} to='/project'>Project</NavLink>
-          <NavLink className={({isActive})=>{
-        return isActive ? 'active-link' : ''
-        }} to='/skills' >Stacks/Skills</NavLink>
+          <NavLink className={({ isActive }) => {
+            return isActive ? 'active-link' : ''
+          }} to='/'>Home</NavLink>
+          <NavLink className={({ isActive }) => {
+            return isActive ? 'active-link' : ''
+          }} to='/project'>Project</NavLink>
+          <NavLink className={({ isActive }) => {
+            return isActive ? 'active-link' : ''
+          }} to='/skills' >Stacks/Skills</NavLink>
         </nav>
       </header>
     </div>
